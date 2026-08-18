d6d83b76f1917062ba971b9535453ff627690fdc
osi2323 <osmanosman3490@gmail.com>
2026-08-19 01:23:55 +0300

    Update Storefront.js

--- components/Storefront.js
+++ components/Storefront.js
@@ -109,7 +109,7 @@ export default function Storefront(){
    }catch(err){setFormError(err.message||'Talep kaydedilemedi.');}
    finally{setRequestLoading(false);}
  }
-return <main><div className="topline">2500 TL üzeri alışverişlerde kargo
+return <main><div className="topline">Bu güne özel alışverişlerinde kargo
 ücretsiz</div><header className="header"><button className="brand ghost"
 onClick={()=>setView('home')}>KARACA</button><div className="search">Aradığın Her
 Şey</div><div className="header-icons"><span>♡
